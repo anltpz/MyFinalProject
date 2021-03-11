6cbb0a4ac70c9fe318af2db763827a0eff296e27
Anıl TOPUZ <aniltopuz@gmail.com>
2021-03-11 15:17:25 +0300

    Core Katmanı Eklendi

---
+++ Core/Entities/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    public interface IEntity// IEntity implement edilen class bir veri tabanı tablosudur
+    {
+
+    }
+}
